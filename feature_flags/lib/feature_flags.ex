346bbf419279a271cf720c8896d99151d30e8a21
Bruno Ferreira <bruno.b.ferreira@tecnico.ulisboa.pt>
2018-10-24 10:10:26 +0100

    Deleted duplicated folder.

--- feature_flags/lib/feature_flags.ex
+++
@@ -1,7 +0,0 @@
-defmodule FeatureFlags do
-  use Application
-
-  def start(_type, _args) do
-    FeatureFlags.Supervisor.start_link(name: FeatureFlags.Supervisor)
-  end
-end
--- feature_flags/test/feature_flags_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FeatureFlagsTest do
-  use ExUnit.Case
-  doctest FeatureFlags
-
-  test "check if bootsrap was successful" do
-    assert :ets.whereis(:feature_table) != :undefined
-  end
-end
